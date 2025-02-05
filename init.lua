f029dafb7783bf7ac415b7ea16a912228a5e5993
HaNa <hampus@hampushuledal.com>
2025-02-05 12:05:10 +0100

    add mappings and commands
    add mapping "<leader>d" show float diagnostic on line
    add autocmd to run build love if it exist
    update plugins

--- init.lua
+++ init.lua
@@ -5,3 +5,13 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.expandtab = false
+
+local love_file = vim.fn.findfile("build_love.sh", ".", 1)
+if love_file ~= "" then
+	vim.api.nvim_create_autocmd("BufWritePost", {pattern = "*.lua", command = ":!bash " .. love_file})
+	print("registered love build autocmd")
+end
+
+vim.keymap.set("n", "<leader>d", function ()
+	vim.diagnostic.open_float()
+end)
