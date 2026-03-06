0d6c535ca8693389294f2ff47aefef36d515d0bf
HaNa <hampus@hampushuledal.com>
2026-03-07 00:12:51 +0100

    disabled coq which does not work on android

--- lua/plugins/lsconfig.lua
+++ lua/plugins/lsconfig.lua
@@ -4,15 +4,15 @@ return {
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			{ "ms-jpq/coq_nvim",       branch = "coq" },
-			{ "ms-jpq/coq.artifacts",  branch = "artifacts" },
-			{ 'ms-jpq/coq.thirdparty', branch = "3p" }
+			--{ "ms-jpq/coq_nvim",       branch = "coq" },
+			--{ "ms-jpq/coq.artifacts",  branch = "artifacts" },
+			--{ 'ms-jpq/coq.thirdparty', branch = "3p" }
 		},
 		init = function()
-			vim.g.coq_settings = {
-				auto_start = "shut-up", -- if you want to start COQ at startup
+			--vim.g.coq_settings = {
+			--	auto_start = "shut-up", -- if you want to start COQ at startup
 				-- Your COQ settings here
-			}
+			--}
 		end,
 		config = function()
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
